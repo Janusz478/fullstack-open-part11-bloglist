3fe819073e1ee99414cf98a668fdf2ce7a87724f
Janusz478 <JFeigel@mailbox.org>
2022-10-21 23:50:03 +0200

    added bloglist files

---
+++ bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,17 @@
+import "@testing-library/jest-dom/extend-expect";
+
+
+describe("Frontend Test", () => {
+  test("Wild Card Test", async () => {
+    const blog = {
+      title: "testTitle",
+      author: "testAuthor",
+      url: "testURL",
+      likes: 0,
+      user: {
+        name: "testUserName",
+      },
+    };
+    expect(blog.title).toEqual("testTitle")
+  });
+});
